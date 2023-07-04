952f0150cf98571d6be459489ae3e1d96bd0c1d7
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2023-07-04 23:32:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function getIpAddress() {
+  fetch("https://api.ipify.org/?format=json")
+    .then((res) => res.json())
+    .then((data) => {
+      const ipAddress = data.ip;
+      document.getElementById("ipLoad").textContent = ipAddress;
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+
+
 function getUserInfo() {
   // Retrieve user's IP address using an API
   fetch("https://api.ipify.org/?format=json")
